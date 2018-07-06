534caf01c42ddb45b624a1368f5e4604a8e28cd4
Daniel Simoes <daniel.simoes@kaffa.com.br>
2018-07-06 11:12:30 -0300

    removed copyright notice

--- src/main/java/com/kaffamobile/kterrorsample/BarJava.java
+++ src/main/java/com/kaffamobile/kterrorsample/BarJava.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (C) Kaffa Mobile - All Rights Reserved
- *
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- */
-
 package com.kaffamobile.kterrorsample;
 
 public final class BarJava {
